d574bd20e2bb0edca13768fefc99fb3783218ef3
MarcoElz <marco.eelizondo@gmail.com>
2017-01-21 01:02:00 -0600

    Shoot, Misil added. Player can Shoot misil.

---
+++ Assets/Scripts/Misil.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Misil : MonoBehaviour {
+
+    private float speed;
+    private float addSpeed;
+
+	// Use this for initialization
+	void Start () 
+    {
+        speed = 1.0f;
+        addSpeed = 0.5f;
+	}
+	
+    void Update()
+    {
+        speed += addSpeed;
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject misil;
+
+    public bool canShoot;
+	// Use this for initialization
+	void Start () 
+    {
+	
+        canShoot = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (canShoot)
+            Shooting();
+	}
+
+    private void Shooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.z = -5.0f;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            pos.z = 0;
+            Debug.Log(pos);
+
+            GameObject misilObject = Instantiate(misil, this.transform.position, Quaternion.identity) as GameObject;
+            misilObject.transform.LookAt(pos);
+
+        }
+            
+    }
+}
